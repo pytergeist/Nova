0501e09d86824dd19a3e2753587b94ccd117bb34
pytergeist <tompopeworks@gmail.com>
2024-11-26 09:56:46 +0000

    removing redundant code from autograd

--- neurothread/tensor/autograd.py
+++ neurothread/tensor/autograd.py
@@ -9,27 +9,27 @@ class Autograd:
         with tensor.lock:
             if tensor.requires_grad:
                 if tensor.grad is None:
-                    tensor.grad = np.ones_like(tensor.data) * grad_output
+                    tensor.grad = grad_output
                 else:
-                    tensor.grad += np.ones_like(tensor.data) * grad_output
+                    tensor.grad += grad_output
         with other.lock:
             if other.requires_grad:
                 if other.grad is None:
-                    other.grad = np.ones_like(other.data) * grad_output
+                    other.grad = grad_output
                 else:
-                    other.grad += np.ones_like(other.data) * grad_output
+                    other.grad += grad_output
 
     @staticmethod
     def subtract_backward(tensor, other, grad_output):
         with tensor.lock:
             if tensor.requires_grad:
                 if tensor.grad is None:
-                    tensor.grad = np.ones_like(tensor.data) * grad_output
+                    tensor.grad = grad_output
                 else:
-                    tensor.grad += np.ones_like(tensor.data) * grad_output
+                    tensor.grad += grad_output
         with other.lock:
             if other.requires_grad:
                 if other.grad is None:
-                    other.grad = -np.ones_like(other.data) * grad_output
+                    other.grad = -grad_output
                 else:
-                    other.grad -= np.ones_like(other.data) * grad_output
+                    other.grad -= grad_output
