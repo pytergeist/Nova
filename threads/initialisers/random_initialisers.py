4053933803d0d9d59ec5e041d289be638aa4a3ef
pytergeist <tompopeworks@gmail.com>
2025-01-29 09:07:38 +0000

    Adding not implamented random/normal initialisers

---
+++ threads/initialisers/random_initialisers.py
@@ -0,0 +1,20 @@
+from threads.initialisers.initialiser import Initialiser
+
+class RandomInitialiser(Initialiser):
+    def __init__(self):
+        raise NotImplementedError
+
+class RandomNormal(object):
+    def __init__(self):
+        raise NotImplementedError
+
+class TruncatedNormal(object):
+    def __init__(self):
+        raise NotImplementedError
+
+
+class RandomUniform(object):
+    def __init__(self):
+        raise NotImplementedError
+
+
