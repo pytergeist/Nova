58ca185fa547d6493c9a74f44882ea13d382ee78
pytergeist <tompopeworks@gmail.com>
2025-02-17 22:15:14 +0000

    Adding finite difference jacobian

--- tests/integration/gradients/finite_difference.py
+++ tests/integration/gradients/finite_difference.py
@@ -20,14 +20,27 @@ def finite_difference(f, x, epsilon):
     return grad
 
 
-def finite_difference_jacobian():
-    pass
+def finite_difference_jacobian(f, x, epsilon=1.5e-8):
+    x = np.atleast_1d(np.asarray(x, dtype=np.float64))
+    f0 = np.atleast_1d(np.asarray(f(x), dtype=np.float64)).ravel()
+    jacobian = np.zeros((f0.size, x.size), dtype=np.float64)
+
+    for i in range(x.size):
+        dx = np.zeros_like(x, dtype=np.float32)
+        dx[i] = epsilon
+
+        f_pos = np.atleast_1d(np.array(f(x + dx), dtype=np.float64)).ravel()
+        f_neg = np.atleast_1d(np.array(f(x - dx), dtype=np.float64)).ravel()
+
+        jacobian[:, i] = (f_pos - f_neg) / (2*epsilon)
+
+    return jacobian
 
 
 def f(x):
-    return x**2
+    return [x**2, x**11]
 
 
 if __name__ == "__main__":
-    x = 10
-    print(finite_difference(f, x, 1.5e-8))
+    x = [10, 10, 10]
+    print(finite_difference_jacobian(f, x, 1.5e-8))
