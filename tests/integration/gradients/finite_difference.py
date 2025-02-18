068dff09213bdec4849109ba90a73d927f9c2de9
pytergeist <tompopeworks@gmail.com>
2025-02-18 10:06:35 +0000

    Adding finalised jacobian calculation function using finite difference

--- tests/integration/gradients/finite_difference.py
+++ tests/integration/gradients/finite_difference.py
@@ -1,30 +1,16 @@
 import numpy as np
 
 
-def finite_difference(f, x, epsilon):
-    """
-    fn to calculate numerical derivative using finite difference approximation
-    This fn only computes scalar gradients for functions, not vectors
-
-    f'(x_i) = (f(x + ε*e_i) - f(x - ε*e_i)) / 2*ε)
-    """
-    if np.isscalar(x) or np.ndim(x) == 0:
-        return (f(x + epsilon) - f(x - epsilon)) / (2 * epsilon)
-
-    x = np.asarray(x)
-    grad = np.zeros_like(x)
-    for i in range(len(x)):
-        dx = np.zeros_like(x)
-        dx[i] = epsilon
-        grad[i] = (f(x + dx) - f(x - dx)) / (2 * epsilon)
-    return grad
-
-
 def finite_difference_jacobian(f, x, epsilon=1.5e-8):
+    """
+    fn to generalise finite difference method for approximating the
+    jacobian matrix using the below equation:
+    f'(x_i) = (f(x + ε*e_i) - f(x - ε*e_i)) / 2*ε)
+    This fn can compute both jacobian and scalar gradients
+    """
     x = np.atleast_1d(np.asarray(x, dtype=np.float64))
     f0 = np.atleast_1d(np.asarray(f(x), dtype=np.float64)).ravel()
-    jacobian = np.zeros((f0.size, x.size), dtype=np.float64)
-
+    jacobian = np.zeros((f0.shape[0], x.size), dtype=np.float64)
     for i in range(x.size):
         dx = np.zeros_like(x, dtype=np.float32)
         dx[i] = epsilon
@@ -32,21 +18,9 @@ def finite_difference_jacobian(f, x, epsilon=1.5e-8):
         f_pos = np.atleast_1d(np.array(f(x + dx), dtype=np.float64)).ravel()
         f_neg = np.atleast_1d(np.array(f(x - dx), dtype=np.float64)).ravel()
 
-        jacobian[:, i] = (f_pos - f_neg) / (2*epsilon)
+        jacobian[:, i] = (f_pos - f_neg) / (2 * epsilon)
 
         if jacobian.size == 1:
             jacobian = jacobian.ravel()
 
     return jacobian
-
-
-def f(x):
-    return [x**2, x**3, x/2, x]
-
-
-if __name__ == "__main__":
-    x = [1.3, .44, 50, 10.4, 1.3]
-    jac = finite_difference_jacobian(f, x, 1.5e-8)
-    print(jac)
-    print(jac.size)
-    print(jac.shape)
