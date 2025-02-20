fixing broken test (x+x+x) - this required zero'ing out gradients when adding (or any operation) the same tensor together multiple times

abditus/tensor/tensor.py
@@ -61,14 +61,15 @@ class Tensor:
             return np.float32
 
     def backward(self, grad_output: Optional[np.ndarray] = None) -> None:
+        self._node._zero_grad() # TODO:
         self._node.backward(grad_output)
 
     def _apply_op(self, other, operation):
         other = other if isinstance(other, Tensor) else Tensor(other)
         out_value = operation.forward_func(self, other)
 
-        out_node = Node(
-            value=out_value,
+        out_node = self.engine.build_node(
+            data=out_value,
             operation=operation,
             parents=(self._node, other._node),
             requires_grad=self._node.requires_grad or other._node.requires_grad,
@@ -111,15 +112,19 @@ class Tensor:
 
 
 if __name__ == "__main__":
-    A = Tensor(np.random.randn(2, 3), requires_grad=True)
-    B = Tensor(np.random.randn(3, 4), requires_grad=True)
+    from abditus.graph.print_graph import print_graph, print_graph_with_grad
+    A = Tensor(np.ones_like([1, 1, 1]), requires_grad=True)
+    B = Tensor(np.ones_like([1, 1, 1]), requires_grad=True)
+    C = Tensor(np.ones_like([1, 1, 1]), requires_grad=True)
 
-    C = A @ B
-    print("Forward: ", C.data)
-
-    loss = C.sum()
-
-    loss.backward()
-
-    print("dLoss/dA = ", A.grad)
-    print("dLoss/dB = ", B.grad)
+    D = A + A + A
+    D.backward()
+    print_graph(D._node)
+    print('-'*10)
+    print_graph_with_grad(D._node)
+    print('='*20)
+    D = A + B + C
+    D.backward()
+    print_graph(D._node)
+    print('-' * 10)
+    print_graph_with_grad(D._node)