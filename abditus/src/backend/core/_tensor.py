7a02f0cfd4dfc3d58746d16ded9485ae2bfa03b9
pytergeist <tompopeworks@gmail.com>
2025-03-01 12:04:31 +0000

    adding docstrings to _tensor.py

--- abditus/src/backend/core/_tensor.py
+++ abditus/src/backend/core/_tensor.py
@@ -1,11 +1,10 @@
-# core
-
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 
 from abditus.src.backend.autodiff import Engine, Node
 from abditus.src.backend.operations import (
+    Operation,
     add_op,
     divide_op,
     matmul_op,
@@ -16,12 +15,29 @@ from abditus.src.backend.operations import (
 
 
 class Tensor:
+    """A Tensor data-structure that uses operator overloading to perform element-wise operations.
+
+    This class represents a tensor that uses operator overloading, registered operations (from the operations
+     module) and the autodiff engine to perform element-wise operations on tensors in the forward pass and track
+     their gradients in the backward pass. It is used as a base class for the Variable class, which
+      is used as a model parameter in the high-level neural network API.
+    .
+
+    Attributes:
+        data (np.ndarray): The underlying data of the tensor.
+        requires_grad (bool): True if gradients should be computed, False otherwise.
+        dtype (np.dtype): The data type of the tensor.
+        engine (Engine): The autodiff engine used to build the computational graph.
+        node (Node): The node in the computational graph that represents the tensor.
+    """
+
     def __init__(
         self,
         data,
         requires_grad=False,
         dtype=np.float32,
-        engine: Engine = Engine(),  # TODO: add engine=None, add get_current() for use with context manager pattern - is this correct?
+        engine: Engine = Engine(),
+        # TODO: add engine=None, add get_current() for use with context manager pattern - is this correct?
         node: Optional[
             Node
         ] = None,  # TODO: should this be here? as the engine manages all nodes
@@ -37,34 +53,72 @@ class Tensor:
 
     @property
     def data(self) -> np.ndarray:
+        """Data property getter. There is no corresponding setter as the data attribute is read-only.
+
+        Returns:
+            np.ndarray: The underlying data of the tensor.
+        """
         return self._node.value
 
     @property
     def grad(self) -> np.ndarray:
+        """Grad (gradient) property getter.
+
+        Returns:
+            np.ndarray: The gradient of the tensor.
+        """
         return self._node.grad
 
     @grad.setter
     def grad(self, new_grad: np.ndarray):
+        """Grad (gradient) property setter.
+
+        Updates the gradient of the tensor.
+        """
         self._node.grad = new_grad
 
     @property
     def requires_grad(self) -> bool:
+        """Grad (gradient) property getter. There is no corresponding setter as the requires_grad
+            attribute is read-only.
+
+        Returns:
+            bool: True if gradients should be computed, False otherwise.
+        """
         return self._node.requires_grad
 
-    @property
-    def value(self):
-        return self._node.value
-
     @staticmethod
-    def standardise_dtype(dtype):  # TODO: add more dtypes, create dtype maps
+    def standardise_dtype(
+        dtype,
+    ):  # TODO: This needs to be abstracted into a separate module
         if dtype is None:
             return np.float32
 
     def backward(self, grad_output: Optional[np.ndarray] = None) -> None:
-        self._node._zero_grad()  # TODO:
+        """Backward pass to compute gradients.
+
+        Args:
+            grad_output (np.ndarray, optional): The gradient of the output tensor. Defaults to None.
+
+        Zero's out the gradients of the current node to prevent double counting
+         and recursively computes gradients for parents.
+        """
+        self._node._zero_grad()
         self._node.backward(grad_output)
 
-    def _apply_op(self, other, operation):
+    def _apply_op(
+        self, other: Union["Tensor", np.ndarray, float, int], operation: Operation
+    ):
+        """Apply a binary operation to this tensor and another tensor or scalar.
+
+        Args:
+            other (Union[Tensor, np.ndarray, float, int]): The other tensor or scalar to apply the operation to.
+            operation (Operation): The operation to apply.
+
+        Returns:
+            Tensor: The result of the operation (a third Tensor).
+
+        """
         other = other if isinstance(other, Tensor) else Tensor(other)
         out_value = operation.forward_func(self, other)
 
@@ -76,7 +130,7 @@ class Tensor:
         )
         return Tensor(data=None, node=out_node)
 
-    def _apply_unary_op(self, operation):
+    def _apply_unary_op(self, operation: Operation) -> "Tensor":
         """
         Like _apply_op, but for single-input (unary) operations.
         """
@@ -89,22 +143,68 @@ class Tensor:
         )
         return Tensor(data=None, node=out_node)
 
-    def __add__(self, other):
+    def __add__(self, other: Union["Tensor", np.ndarray, float, int]) -> "Tensor":
+        """Addition operator overload.
+
+        args:
+            other (Union[Tensor, np.ndarray, float, int]): The other tensor or scalar to add.
+
+        Returns:
+            Tensor: The result of the addition.
+        """
         return self._apply_op(other, add_op)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Union["Tensor", np.ndarray, float, int]) -> "Tensor":
+        """Subtraction operator overload.
+
+        args:
+            other (Union[Tensor, np.ndarray, float, int]): The other tensor or scalar to subtract.
+
+        Returns:
+            Tensor: The result of the subtraction.
+        """
         return self._apply_op(other, subtract_op)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other: Union["Tensor", np.ndarray, float, int]) -> "Tensor":
+        """Right subtraction operator overload.
+
+        args:
+            other (Union[Tensor, np.ndarray, float, int]): The other tensor or scalar to subtract.
+
+        Returns:
+            Tensor: The result of the subtraction.
+        """
         return self._apply_op(other, right_subtract_op)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Union["Tensor", np.ndarray, float, int]) -> "Tensor":
+        """Division operator overload.
+
+        args:
+            other (Union[Tensor, np.ndarray, float, int]): The other tensor or scalar to divide.
+
+        Returns:
+            Tensor: The result of the division.
+        """
         return self._apply_op(other, divide_op)
 
-    def __matmul__(self, other):
+    def __matmul__(self, other: Union["Tensor", np.ndarray]) -> "Tensor":
+        """Matrix multiplication operator overload.
+
+        args:
+            other (Union[Tensor, np.ndarray): The other tensor or array.
+
+        Returns:
+            Tensor: The result of the matrix multiplication
+
+        """
         return self._apply_op(other, matmul_op)
 
     def sum(self):
+        """Sum the elements of the tensor.
+
+        Returns:
+            Tensor: The sum of the tensor elements.
+        """
         return self._apply_unary_op(sum_op)
 
     def __repr__(self):
