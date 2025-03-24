bfd9c527f8c68631db8edbd0a3c2567062d34808
pytergeist <tompopeworks@gmail.com>
2025-03-24 11:06:33 +0000

    Adding pow to tensor class, using std::pow and lambda fn

--- fusion/src/Fusion/core/tensor.h
+++ fusion/src/Fusion/core/tensor.h
@@ -1,38 +1,34 @@
 #ifndef TENSOR_H
 #define TENSOR_H
 
-
-
 #include <iostream>
 #include <vector>
 
+template <typename T> class Tensor {
+public:
+  std::vector<T> arr;
 
-template <typename T>
-class Tensor {
-  public:
-    std::vector<T> arr;
+  explicit Tensor(const std::vector<T> &data);
 
-    explicit Tensor(const std::vector<T> &data);
+  explicit Tensor(const T &value);
 
-    explicit Tensor(const T &value);
+  friend std::ostream &operator<<(std::ostream &os, const Tensor &tensor) {
+    os << "Tensor(";
+    for (size_t i = 0; i < tensor.arr.size(); ++i) {
+      os << tensor.arr[i];
+      if (i < tensor.arr.size() - 1)
+        os << ", ";
+    }
+    os << ")";
+    return os;
+  }
 
-    friend std::ostream &operator<<(std::ostream &os, const Tensor &tensor) {
-        os << "Tensor(";
-        for (size_t i = 0; i < tensor.arr.size(); ++i) {
-          os << tensor.arr[i];
-          if (i < tensor.arr.size() - 1)
-            os << ", ";
-        }
-        os << ")";
-        return os;
-      }
-
-      Tensor<T> operator+(const Tensor<T> &tensor) const;
-      Tensor<T> operator-(const Tensor<T> &tensor) const;
-      Tensor<T> operator*(const Tensor<T> &tensor) const;
-      Tensor<T> operator/(const Tensor<T> &tensor) const;
-
+  Tensor<T> operator+(const Tensor<T> &tensor) const;
+  Tensor<T> operator-(const Tensor<T> &tensor) const;
+  Tensor<T> operator*(const Tensor<T> &tensor) const;
+  Tensor<T> operator/(const Tensor<T> &tensor) const;
+  Tensor<T> pow(const Tensor<T> &tensor) const;
 };
 
-#include "tensor.tpp"
-#endif //TENSOR_H
+#include "tensor_ops.tpp"
+#endif // TENSOR_H
