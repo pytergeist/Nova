89dda3e3ef8ca4cf36073655fcb959a3183017c2
pytergeist <tompopeworks@gmail.com>
2025-03-26 22:03:14 +0000

    Adding ostream printing for 1d/2d tensors

--- fusion/src/Fusion/core/main.cpp
+++ fusion/src/Fusion/core/main.cpp
@@ -5,6 +5,9 @@ int main() {
   Tensor<double> const tensor2({1.0, 2.0, 3.0, 4.0, 5.0});
   Tensor<double> const &tensor3 = tensor2.log();
 
-  std::cout << tensor3 << std::endl;
+  Tensor<std::vector<double>> const tensor4(
+      {{1.0, 2.0, 4.0, 8.0, 6.0}, {1.0, 2.0, 3.0, 4.0, 5.0}});
+
+  std::cout << tensor4 << std::endl;
   return 0;
 }
--- fusion/src/Fusion/core/tensor.h
+++ fusion/src/Fusion/core/tensor.h
@@ -4,34 +4,74 @@
 #include <iostream>
 #include <vector>
 
+template <typename> struct is_std_vector : std::false_type {};
+
+template <typename T, typename A>
+struct is_std_vector<std::vector<T, A>> : std::true_type {};
+
 template <typename T> class Tensor {
 public:
   std::vector<T> arr;
 
   explicit Tensor(const std::vector<T> &data);
 
+  // explicit Tensor(const std::vector<std::vector<T>> &data);
+
   explicit Tensor(const T &value);
+
+  // friend std::ostream &operator<<(std::ostream &os, const Tensor &tensor) {
+  //   os << "Tensor(";
+  //   for (size_t i = 0; i < tensor.arr.size(); ++i) {
+  //     os << tensor.arr[i];
+  //     if (i < tensor.arr.size() - 1)
+  //       os << ", ";
+  //   }
+  //   os << ")";
+  //   return os;
+  // }
 
   friend std::ostream &operator<<(std::ostream &os, const Tensor &tensor) {
     os << "Tensor(";
-    for (size_t i = 0; i < tensor.arr.size(); ++i) {
-      os << tensor.arr[i];
-      if (i < tensor.arr.size() - 1)
-        os << ", ";
+    os << std::endl;
+    if constexpr (is_std_vector<T>::value) {
+      for (size_t i = 0; i < tensor.arr.size(); ++i) {
+        for (size_t j = 0; j < tensor.arr[i].size(); ++j) {
+          os << tensor.arr[i][j];
+          if (j < tensor.arr[i].size() - 1)
+            os << ", ";
+        }
+        os << std::endl;
+      }
+      os << ")";
+      return os;
     }
-    os << ")";
-    return os;
+    if constexpr (!is_std_vector<T>::value) {
+      for (size_t i = 0; i < tensor.arr.size(); ++i) {
+        os << tensor.arr[i];
+        if (i < tensor.arr.size() - 1)
+          os << ", ";
+      }
+      os << ")";
+      return os;
+    }
   }
+
   // binary operations
   Tensor<T> operator+(const Tensor<T> &tensor) const;
+
   Tensor<T> operator-(const Tensor<T> &tensor) const;
+
   Tensor<T> operator*(const Tensor<T> &tensor) const;
+
   Tensor<T> operator/(const Tensor<T> &tensor) const;
+
   Tensor<T> pow(const Tensor<T> &tensor) const;
 
   // unary operations
   Tensor<T> sqrt() const;
+
   Tensor<T> exp() const;
+
   Tensor<T> log() const;
 };
 
