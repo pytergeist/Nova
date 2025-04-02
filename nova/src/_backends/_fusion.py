87bc50e87f2eca8d8fc48dd43aa9653b5aca5dbb
pytergeist <tompopeworks@gmail.com>
2025-04-02 19:52:05 +0100

    Adding fusion backend skeleton class

--- nova/src/_backends/_fusion.py
+++ nova/src/_backends/_fusion.py
@@ -1,10 +1,51 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import numpy as np
+
+
 class FusionBackend:
     def __init__(self):
-        self.fusion = None
+        self.backend = None
         try:
             import fusion
-            self.fusion = True
+            self.backend = fusion
         except ImportError:
             raise ImportError("Fusion backend not found. Please install the fusion package.")
 
+    def add(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.add(v1, v2)
 
+    def subtract(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.subtract(v1, v2)
+
+    def multiply(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.multiply(v1, v2)
+
+    def divide(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.divide(v1, v2)
+
+    @staticmethod
+    def matmul(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.matmul(v1, v2)
+
+    def sum(self, v1: "np.ndarray") -> "np.ndarray":
+        return self.backend.sum(v1)
+
+    def maximum(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.maximum(v1, v2)
+
+    def transpose(self, v1: "np.ndarray") -> "np.ndarray":
+        return self.backend.transpose(v1)
+
+    def power(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
+        return self.backend.power(v1, v2)
+
+    def exp(self, v1: "np.ndarray") -> "np.ndarray":
+        return self.backend.exp(v1)
+
+    def log(self, v1: "np.ndarray") -> "np.ndarray":
+        return self.backend.log(v1)
+
+    def sqrt(self, v1: "np.ndarray") -> "np.ndarray":
+        return self.backend.sqrt(v1)
