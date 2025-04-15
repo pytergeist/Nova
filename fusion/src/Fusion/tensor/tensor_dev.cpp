e593e45b79b947948a8af1bdf3bd4ff37303b793
pytergeist <tompopeworks@gmail.com>
2025-04-15 09:24:33 +0100

    Adding matmul and transpose to tensor_dev.cpp class

--- fusion/src/Fusion/tensor/tensor_dev.cpp
+++ fusion/src/Fusion/tensor/tensor_dev.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <memory>
 #include <stdexcept>
+#include <cblas.h>
 
 template <typename T> class Tensor {
 public:
@@ -245,16 +246,55 @@ public:
 
     throw std::invalid_argument("Tensor shapes do not match for maximum");
   }
+
+    Tensor<T> matmul(const Tensor<T>& other) const {
+      if (this->storage->cols() != other.storage->rows()) {
+          throw std::invalid_argument("Matrix dimension mismatch for matmul");
+      }
+      const auto *cpuThis = dynamic_cast<const EigenTensorStorage<T>*>(this->storage.get());
+      const auto *cpuOther = dynamic_cast<const EigenTensorStorage<T>*>(other.storage.get());
+      if (!cpuThis || !cpuOther) {
+          throw std::invalid_argument("Unsupported storage type for matmul");
+      }
+      int m = static_cast<int>(cpuThis->matrix.rows());
+      int k = static_cast<int>(cpuThis->matrix.cols()); // same as other.rows()
+      int n = static_cast<int>(cpuOther->matrix.cols());
+
+      Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> resultMat(m, n);
+
+      const double alpha = 1.0;
+      const double beta  = 0.0;
+
+      cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                  m, n, k,
+                  alpha,
+                  cpuThis->matrix.data(), k,
+                  cpuOther->matrix.data(), n,
+                  beta,
+                  resultMat.data(), n);
+
+      return Tensor<T>(resultMat);
+  }
+
+    Tensor<T> transpose() const {
+      const auto *cpuThis = dynamic_cast<const EigenTensorStorage<T>*>(this->storage.get());
+      if (!cpuThis) {
+          throw std::runtime_error("Unsupported storage type for transpose");
+      }
+      Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> transposedMat =
+        cpuThis->matrix.transpose();
+      return Tensor<T>(transposedMat);
+  }
 };
 
 int main() {
   Tensor<double> tensorA(2, 2, Device::CPU);
-  Tensor<double> tensorB(1, 1, Device::CPU);
+  Tensor<double> tensorB(2, 2, Device::CPU);
 
   tensorA.setValues({1, 2, 3, 9});
-  tensorB.setValues({9});
+  tensorB.setValues({1, 2, 3, 9});
 
-  Tensor<double> tensorC = tensorA.maximum(tensorB);
+  Tensor<double> tensorC = tensorA.transpose();
 
   std::cout << "tensorA:\n" << tensorA << std::endl;
   std::cout << "tensorB:\n" << tensorB << std::endl;
