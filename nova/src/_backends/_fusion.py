Fixing all but three tests New eigen tensor backend causing identity matrix*grad vector (n,m) jacobian - need to enforce standard shapes Log, Exp, and maximum fn's in fusion backend are causing test errors

nova/src/_backends/_fusion.py
@@ -22,7 +22,11 @@ class FusionBackend:
 
     def _convert_numpy_to_tensor(self, array: "np.ndarray") -> "fusion_math.Tensor":
         """Convert numpy arrays to fusion tensors."""
-        fusion_tensor = self.backend.Tensor(array.flatten(), array.shape)
+        shape = list(array.shape)
+        if len(shape) == 1:
+            shape = [shape[0], 1]
+        data = list(array.flatten().astype(float))
+        fusion_tensor = self.backend.Tensor(shape, data)
         return fusion_tensor
 
     def _convert_scalar_to_tensor(self, scalar: "np.float32") -> "fusion_math.Tensor":
@@ -32,8 +36,9 @@ class FusionBackend:
 
     @staticmethod
     def _convert_tensor_to_numpy(tensor: "fusion_math.Tensor") -> "np.ndarray":
-        """Convert fusion tensors to numpy arrays."""
-        return tensor.to_numpy()
+        return (
+            tensor.to_numpy()
+        )  # TODO: this is causing shape mismatch issues - squeezing in tests for now
 
     def add(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
         tensor1 = self._convert_numpy_to_tensor(v1)
@@ -84,7 +89,7 @@ class FusionBackend:
     def power(self, v1: "np.ndarray", v2: "np.ndarray") -> "np.ndarray":
         tensor1 = self._convert_numpy_to_tensor(v1)
         tensor2 = self._convert_numpy_to_tensor(v2)
-        tensor3 = tensor2.pow(tensor1)
+        tensor3 = tensor1.pow(tensor2)
         return self._convert_tensor_to_numpy(tensor3)
 
     def exp(self, v1: "np.ndarray") -> "np.ndarray":

nova/src/backend/autodiff/_node.py
@@ -102,11 +102,19 @@ class Node:
         return self._parents
 
     def update_node_gradient(self, grad_output: Optional[np.ndarray] = None) -> None:
-        """Update the gradient of the node.
-
-        Args:
-            grad_output (Optional[np.ndarray]): The gradient of the output tensor.
+        """Update the gradient of the node ensuring that the gradient
+        matches the shape of the node's value.
         """
+        if grad_output is None:
+            return
+
+        expected_shape = (
+            self.value.shape
+        )  # TODO: why tf has changing to an eigen backend caused jacobian to be an identity matrix*grad_vector
+        if grad_output.shape != expected_shape:
+            if grad_output.ndim == 2:
+                grad_output = np.diag(grad_output)
+
         if self._grad is None:
             self._grad = grad_output
         else:

tests/unit/tensor/test_ops.py
@@ -15,9 +15,9 @@ from nova.src.backend.operations import (
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data, requires_grad",
     [
-        ([1, 2, 3], [4, 5, 6], [5, 7, 9], False),
-        ([0, 0, 0], [1, 1, 1], [1, 1, 1], False),
-        ([1, -1, 1], [-1, 1, -1], [0, 0, 0], False),
+        ([1, 2, 3], [4, 5, 6], np.array([5, 7, 9]).reshape(-1, 1), False),
+        ([0, 0, 0], [1, 1, 1], np.array([1, 1, 1]).reshape(-1, 1), False),
+        ([1, -1, 1], [-1, 1, -1], np.array([0, 0, 0]).reshape(-1, 1), False),
     ],
 )
 def test_add(data_a, data_b, expected_data, requires_grad):
@@ -34,9 +34,9 @@ def test_add(data_a, data_b, expected_data, requires_grad):
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data, requires_grad",
     [
-        ([1, 2, 3], [4, 5, 6], [-3, -3, -3], False),
-        ([0, 0, 0], [1, 1, 1], [-1, -1, -1], False),
-        ([1, -1, 1], [-1, 1, -1], [2, -2, 2], False),
+        ([1, 2, 3], [4, 5, 6], np.array([-3, -3, -3]).reshape(-1, 1), False),
+        ([0, 0, 0], [1, 1, 1], np.array([-1, -1, -1]).reshape(-1, 1), False),
+        ([1, -1, 1], [-1, 1, -1], np.array([2, -2, 2]).reshape(-1, 1), False),
     ],
 )
 def test_subtract(data_a, data_b, expected_data, requires_grad):
@@ -53,9 +53,9 @@ def test_subtract(data_a, data_b, expected_data, requires_grad):
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data, requires_grad",
     [
-        ([1, 2, 3], [4, 5, 6], [3, 3, 3], False),
-        ([0, 0, 0], [1, 1, 1], [1, 1, 1], False),
-        ([1, -1, 1], [-1, 1, -1], [-2, 2, -2], False),
+        ([1, 2, 3], [4, 5, 6], np.array([3, 3, 3]).reshape(-1, 1), False),
+        ([0, 0, 0], [1, 1, 1], np.array([1, 1, 1]).reshape(-1, 1), False),
+        ([1, -1, 1], [-1, 1, -1], np.array([-2, 2, -2]).reshape(-1, 1), False),
     ],
 )
 def test_right_subtract(data_a, data_b, expected_data, requires_grad):
@@ -72,9 +72,9 @@ def test_right_subtract(data_a, data_b, expected_data, requires_grad):
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data, requires_grad",
     [
-        ([1, 2, 3], [4, 5, 6], [0.25, 0.4, 0.5], False),
-        ([0, 0, 0], [1, 1, 1], [0.0, 0.0, 0.0], False),
-        ([1, -2, 3], [-1, 2, -3], [-1.0, -1.0, -1.0], False),
+        ([1, 2, 3], [4, 5, 6], np.array([0.25, 0.4, 0.5]).reshape(-1, 1), False),
+        ([0, 0, 0], [1, 1, 1], np.array([0.0, 0.0, 0.0]).reshape(-1, 1), False),
+        ([1, -2, 3], [-1, 2, -3], np.array([-1.0, -1.0, -1.0]).reshape(-1, 1), False),
     ],
 )
 def test_true_div(data_a, data_b, expected_data, requires_grad):

tests/unit/tensor/test_tensor.py
@@ -7,9 +7,9 @@ from nova.src.backend.core import Tensor
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data",
     [
-        ([1, 2, 3], [4, 5, 6], [5, 7, 9]),
-        ([0, 0, 0], [1, 1, 1], [1, 1, 1]),
-        ([1, -1, 1], [-1, 1, -1], [0, 0, 0]),
+        ([1, 2, 3], [4, 5, 6], np.array([5, 7, 9]).reshape(-1, 1)),
+        ([0, 0, 0], [1, 1, 1], np.array([1, 1, 1]).reshape(-1, 1)),
+        ([1, -1, 1], [-1, 1, -1], np.array([0, 0, 0]).reshape(-1, 1)),
     ],
 )
 def test_tensor_addition(data_a, data_b, expected_data):
@@ -23,9 +23,9 @@ def test_tensor_addition(data_a, data_b, expected_data):
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data",
     [
-        ([1, 2, 3], [4, 5, 6], [-3, -3, -3]),
-        ([0, 0, 0], [1, 1, 1], [-1, -1, -1]),
-        ([1, -1, 1], [-1, 1, -1], [2, -2, 2]),
+        ([1, 2, 3], [4, 5, 6], np.array([-3, -3, -3]).reshape(-1, 1)),
+        ([0, 0, 0], [1, 1, 1], np.array([-1, -1, -1]).reshape(-1, 1)),
+        ([1, -1, 1], [-1, 1, -1], np.array([2, -2, 2]).reshape(-1, 1)),
     ],
 )
 def test_tensor_subtraction(data_a, data_b, expected_data):
@@ -39,9 +39,9 @@ def test_tensor_subtraction(data_a, data_b, expected_data):
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data",
     [
-        ([1, 2, 3], [4, 5, 6], [3, 3, 3]),
-        ([0, 0, 0], [1, 1, 1], [1, 1, 1]),
-        ([1, -1, 1], [-1, 1, -1], [-2, 2, -2]),
+        ([1, 2, 3], [4, 5, 6], np.array([3, 3, 3]).reshape(-1, 1)),
+        ([0, 0, 0], [1, 1, 1], np.array([1, 1, 1]).reshape(-1, 1)),
+        ([1, -1, 1], [-1, 1, -1], np.array([-2, 2, -2]).reshape(-1, 1)),
     ],
 )
 def test_tensor_right_subtraction(data_a, data_b, expected_data):
@@ -108,9 +108,9 @@ def test_tensor_backward_right_subtraction(
 @pytest.mark.parametrize(
     "data_a, data_b, expected_data",
     [
-        ([1, 2, 3], [4, 5, 6], [0.25, 0.40, 0.50]),
-        ([0, 0, 0], [1, 1, 1], [0.0, 0.0, 0.0]),
-        ([2, -4, 10], [-2, 2, 5], [-1.0, -2.0, 2.0]),
+        ([1, 2, 3], [4, 5, 6], np.array([0.25, 0.40, 0.50]).reshape(-1, 1)),
+        ([0, 0, 0], [1, 1, 1], np.array([0.0, 0.0, 0.0]).reshape(-1, 1)),
+        ([2, -4, 10], [-2, 2, 5], np.array([-1.0, -2.0, 2.0]).reshape(-1, 1)),
     ],
 )
 def test_tensor_division(data_a, data_b, expected_data):