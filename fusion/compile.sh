c0d58293387b37513f2140a5d20a728cd0a2cb79
pytergeist <tompopeworks@gmail.com>
2025-04-24 09:38:09 +0100

    Adding cd to bash source dir to compile script

--- fusion/compile.sh
+++ fusion/compile.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 rm -rf build
 mkdir build
-cd build || exit
-cmake -DPython_EXECUTABLE:FILEPATH="$(which python)" ..
-cmake -DCMAKE_PREFIX_PATH="/opt/homebrew" ..
+cd build
+
+# now this .. refers to fusion/CMakeLists.txt
+cmake -DPython_EXECUTABLE="$(which python)" ..
 make
