9f2b4f21d3541ae04c4c204ecedf9790bc81f633
pytergeist <tompopeworks@gmail.com>
2025-06-19 15:20:27 +0100

    Updating call in ReLU fn

--- nova/src/blocks/activations/activations.py
+++ nova/src/blocks/activations/activations.py
@@ -6,7 +6,7 @@ class ReLU(Block):
     def __init__(self):
         super().__init__()
 
-    def __call__(self, inputs: Tensor, **kwargs):
+    def call(self, inputs: Tensor, **kwargs):
         return inputs.maximum(0)
 
     def get_config(self):
