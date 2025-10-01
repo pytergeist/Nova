3bf1b4200c9fc462964c39d3ee4ba05174979c32
pytergeist <tompopeworks@gmail.com>
2025-10-01 21:08:44 +0100

    Adding MultiTensor type to autodiff/Traits.h

--- fusion/src/Fusion/autodiff/Traits.h
+++ fusion/src/Fusion/autodiff/Traits.h
@@ -6,4 +6,23 @@
 struct ValueID {uint16_t idx;};
 struct NodeID {int16_t idx;};
 
+template <typename T>
+struct MultiTensor {
+  std::vector<std::vector<T>> data;
+
+  MultiTensor() = default;
+  explicit MultiTensor(std::size_t n) : data(n) {};
+
+  std::size_t size() const noexcept { return data.size(); };
+  std::vector<T>& operator[](std::size_t i) { return data.at(i); };
+  const std::vector<T>& operator[](std::size_t i) const noexcept { return data.at(i); };
+
+
+  auto begin() {return data.begin();};
+  auto end() {return data.begin();};
+  auto begin() const {return data.begin();};
+  auto end() const {return data.begin();};
+
+};
+
 #endif // TRAITS_H
