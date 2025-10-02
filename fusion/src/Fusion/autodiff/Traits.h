b529e2bf7378781cc370870f93fab6f683a66e6c
pytergeist <tompopeworks@gmail.com>
2025-10-02 18:17:00 +0100

    fixing iter bugs in MultiTensor

--- fusion/src/Fusion/autodiff/Traits.h
+++ fusion/src/Fusion/autodiff/Traits.h
@@ -2,11 +2,14 @@
 #define TRAITS_H
 
 #include <cstdint>
+#include <vector>
+#include <initializer_list>
 
 // TODO: Create fixed size multitensor for hot paths
 
-struct ValueID {uint16_t idx;};
-struct NodeID {int16_t idx;};
+
+struct ValueID { uint16_t idx; };
+struct NodeID  { int16_t  idx; };
 
 template <typename T>
 struct MultiTensor {
@@ -14,28 +17,28 @@ struct MultiTensor {
 
   MultiTensor() = default;
 
-  explicit MultiTensor(std::size_t n) : data(n) {};
-  explicit MultiTensor(std::vector<std::vector<T>> data) : data(std::move(data)) {};
-  MultiTensor(std::initializer_list<std::initializer_list<T>> init) : data(std::move(init)) {
-       data.reserve(init.size());
-       for (auto& il : init) {
-         data.emplace_back(il);
-       }
-  };
+  explicit MultiTensor(std::size_t n) : data(n) {}
 
-  MultiTensor(std::initializer_list<std::vector<T>> init) : data(std::move(init)) {}
+  MultiTensor(std::initializer_list<std::initializer_list<T>> init) {
+    data.reserve(init.size());
+    for (const auto& il : init) {
+      data.emplace_back(il.begin(), il.end());
+    }
+  }
 
+  MultiTensor(std::initializer_list<std::vector<T>> init) : data(init) {}
 
-  std::size_t size() const noexcept { return data.size(); };
-  std::vector<T>& operator[](std::size_t i) { return data.at(i); };
-  const std::vector<T>& operator[](std::size_t i) const noexcept { return data.at(i); };
+  void push_back(std::vector<T> v) { data.push_back(std::move(v)); }
 
+  std::size_t size() const noexcept { return data.size(); }
 
-  auto begin() {return data.begin();};
-  auto end() {return data.begin();};
-  auto begin() const {return data.begin();};
-  auto end() const {return data.begin();};
+  std::vector<T>&       operator[](std::size_t i)       { return data.at(i); }
+  const std::vector<T>& operator[](std::size_t i) const { return data.at(i); }
 
+  auto begin()       { return data.begin(); }
+  auto end()         { return data.end();   }
+  auto begin() const { return data.begin(); }
+  auto end()   const { return data.end();   }
 };
 
 #endif // TRAITS_H
