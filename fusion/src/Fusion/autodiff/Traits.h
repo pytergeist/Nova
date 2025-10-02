20f72756650843f5c4d01066e9d45f38a3c25850
pytergeist <tompopeworks@gmail.com>
2025-10-02 09:22:02 +0100

    Adding constructors that use initialiser lists in MultiTensor first class type

--- fusion/src/Fusion/autodiff/Traits.h
+++ fusion/src/Fusion/autodiff/Traits.h
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 
+// TODO: Create fixed size multitensor for hot paths
+
 struct ValueID {uint16_t idx;};
 struct NodeID {int16_t idx;};
 
@@ -11,7 +13,18 @@ struct MultiTensor {
   std::vector<std::vector<T>> data;
 
   MultiTensor() = default;
+
   explicit MultiTensor(std::size_t n) : data(n) {};
+  explicit MultiTensor(std::vector<std::vector<T>> data) : data(std::move(data)) {};
+  MultiTensor(std::initializer_list<std::initializer_list<T>> init) : data(std::move(init)) {
+       data.reserve(init.size());
+       for (auto& il : init) {
+         data.emplace_back(il);
+       }
+  };
+
+  MultiTensor(std::initializer_list<std::vector<T>> init) : data(std::move(init)) {}
+
 
   std::size_t size() const noexcept { return data.size(); };
   std::vector<T>& operator[](std::size_t i) { return data.at(i); };
