74405257e430ff082401f59c5434ad2592fdd5b9
pytergeist <tompopeworks@gmail.com>
2025-10-15 17:12:04 +0100

    Adding autodiffMode with noGradGuard, preventing cyclic calls to graph

---
+++ fusion/src/Fusion/autodiff/AutodiffMode.h
@@ -0,0 +1,17 @@
+#ifndef AUTODIFF_MODE_H
+#define AUTODIFF_MODE_H
+
+
+namespace autograd {
+    inline thread_local bool g_enable_grad = true;
+
+    struct NoGradGuard {
+        bool prev_;
+        NoGradGuard() : prev_(g_enable_grad) { g_enable_grad = false; }
+        ~NoGradGuard() { g_enable_grad = prev_; }
+    };
+
+    inline bool grad_enabled() { return g_enable_grad; }
+}
+
+#endif // AUTODIFF_MODE_H
