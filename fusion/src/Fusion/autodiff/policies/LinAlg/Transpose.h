d3bdd6188770fadae102170c368a11f7cd01bf54
pytergeist <tompopeworks@gmail.com>
2025-10-21 09:38:55 +0100

    Updating matmul policy with inner dims check

--- fusion/src/Fusion/autodiff/policies/LinAlg/Transpose.h
+++ fusion/src/Fusion/autodiff/policies/LinAlg/Transpose.h
@@ -9,8 +9,8 @@
 #include "../../common/Log.h"
 
 template <typename T>
-struct MatMul {
-    inline static constexpr std::string_view name = "MatMul";
+struct Transpose {
+    inline static constexpr std::string_view name = "Transpose";
     using In = MultiTensor<T>;
     using Out = MultiTensor<T>;
     using GradIn = MultiTensor<T>;
@@ -18,20 +18,15 @@ struct MatMul {
 
     Out forward(Context<T>& context, In& input) {
         autodiff::NoGradGuard _;
-        FUSION_CHECK(input.size() >= 2, "MatMul requires two inputs");
+        FUSION_CHECK(input.size() >= 2, "Transpose requires two inputs");
         FUSION_BOUNDS_CHECK(0, input.size());
         FUSION_BOUNDS_CHECK(1, input.size());
         const auto& a = input[0];
         const auto& b = input[1];
-        FUSION_LOGI("MatMul::forward  a.shape=", a.shape_str(),
-            " b.shape=", b.shape_str(),
-            " a.rank=", a.rank(), " b.rank=", b.rank(), " a.ndims=", a.ndims(), " b.ndims=", b.ndims());
         context.save("a", a);
         context.save("b", b);
-        FUSION_CHECK(a.rank() >= 2 && b.rank() >= 2, "MatMul: rank must be >= 2");
         const auto K_a = a.shape_.back();
   		const auto K_b = b.shape_[b.rank_ - 2];
-  		FUSION_CHECK(K_a == K_b, "MatMul: inner dims mismatch");
         Tensor<T> c = a.matmul(b);
         Out out;
         out.push_back(c);
