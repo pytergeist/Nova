cb66f62231db7e8cd8ea9dbb15b756283f8b0451
pytergeist <tompopeworks@gmail.com>
2025-11-01 10:49:14 +0000

    Adding empty tensor initialiser method and passing buffer in add kernel instead of vec obj

--- fusion/src/Fusion/ops/Ewise.h
+++ fusion/src/Fusion/ops/Ewise.h
@@ -11,13 +11,27 @@
 namespace math {
 
 template <typename T>
-inline Tensor<T> add(const Tensor<T> &x, const Tensor<T> &y) {
-   std::vector<size_t> out_shape;
-   std::vector<T> out_data;
-   ewise::binary_ewise_tag<T, AddSIMD>(x, y, out_shape, out_data);
-   return Tensor<T>(std::move(out_shape), std::move(out_data), Device::CPU,
-                    grad_flow(x, y));
+inline Tensor<T> init_empty_tensor(const Tensor<T> &x, const Tensor<T> &y) {
+
+   return Tensor<T>(x.shape(), Device::CPU, grad_flow(x, y));
 }
+
+template <typename T>
+inline Tensor<T> add(const Tensor<T> &x, const Tensor<T> &y) {
+    std::vector<size_t> out_shape;
+    Tensor<T> out = init_empty_tensor(x, y);
+    ewise::binary_ewise_tag<T, AddSIMD>(x, y, out_shape, out);
+    return out;
+}
+
+//template <typename T>
+//inline Tensor<T> add(const Tensor<T> &x, const Tensor<T> &y) {
+//   std::vector<size_t> out_shape;
+//   std::vector<T> out_data;
+//   ewise::binary_ewise_tag<T, AddSIMD>(x, y, out_shape, out_data);
+//   return Tensor<T>(std::move(out_shape), std::move(out_data), Device::CPU,
+//                    grad_flow(x, y));
+//}
 
 template <typename T>
 inline Tensor<T> sub(const Tensor<T> &x, const Tensor<T> &y) {
