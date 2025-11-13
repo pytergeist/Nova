6fad79a946115a8e687673ed8323bd3f49f21061
pytergeist <tompopeworks@gmail.com>
2025-11-13 13:21:23 +0000

    Refactoring pool to use tensor BFCAllocator style design pattern, adding Bucket obj, that represents raw memory measured in bytes

--- fusion/src/Fusion/alloc/Pool.h
+++ fusion/src/Fusion/alloc/Pool.h
@@ -2,133 +2,46 @@
 #define POOL_H
 
 #include <cstddef>
+#include <cstdint>
+#include <utility>
 
-// implamentation of constant sixed sized mem bucket
+/* This header file includes code for the memory pool used by the pool
+allocator, the pool consists of bucketss, which point to mem regions - the
+buckets store various meta-data, which describes whether a bucket is in use,
+what the next/previous mem region is, the ptr to the curr mem region. */
 
-class Bucket {
-   public:
+using BucketId = std::size_t;
+static const BucketId kInvalidBucketId = static_cast<BucketId>(-1);
 
-     Bucket(std::size_t block_size, std::size_t block_count) : block_size_(block_size), block_count_(block_count) {
-        const auto data_size = block_size * block_count;
-        mem_data_ = static_cast<std::byte*>(std::malloc(data_size));
-        assert(mem_data_ != nullptr);
-        const auto ledger_size = 1 + ((block_count - 1) / 8);
-        mem_ledger_ = static_cast<std::byte *>(std::malloc(ledger_size));
-        assert(mem_ledger_ != nullptr);
-        std::memset(mem_data_, 0, data_size);
-        std::memset(mem_ledger_, 0, ledger_size);
-     }
+struct Bucket {
+ public:
 
-     ~Bucket() {
-        std::free(mem_ledger_);
-        std::free(mem_data_);
-     }
+   bool in_use() const noexcept { return std::cmp_not_equal(allocation_id_, kInvalidBucketId); }
 
+   std::size_t size() const noexcept { return size_; }
+   std::size_t requested_size() const noexcept { return requested_size_; }
+   std::int64_t allocation_id() const noexcept { return allocation_id_; }
 
-     std::size_t block_size() const noexcept {return block_size_;}
-     std::size_t block_count() const noexcept {return block_count_;}
+   void set_size(std::size_t bucket_size) noexcept { size_ = bucket_size; }
+   void set_requested_size(std::size_t req_bucket_size) noexcept {
+      requested_size_ = req_bucket_size;
+   }
+   void set_allocation_id(std::int64_t alloc_id) noexcept {
+      allocation_id_ = alloc_id;
+   }
 
-     bool belongs(void* ptr) const noexcept;
+   BucketId prev() const noexcept { return prev_; }
+   BucketId next() const noexcept { return next_; }
 
-     [[nodiscard]] void* allocate(std::size_t bytes) noexcept {
-        const auto n = 1 + ((bytes - 1) / block_size()); // calc num blocks
+ private:
+   void *ptr = nullptr; // ptr to mem
+   std::size_t size_ = 0;           // size of buffer
+   std::size_t requested_size_ = 0; // the client requested size of the buffer
+   std::int64_t allocation_id_ = -1;
 
-        const auto index  = find_contiguous_blocks(n);
-        if (index == block_count()) {
-           return nullptr;
-        }
-
-        set_blocks_in_use(index, n);
-        return mem_data_ + (index * block_size());
-
-     };
-
-
-     void deallocate(void* ptr, std::size_t bytes) noexcept {
-        const auto p = static_cast<const std::byte *>(ptr);
-        const std::size_t dist = static_cast<std::size_t>(p - mem_data_);
-        const auto index = dist / block_size();
-        const auto n = 1 + ((bytes - 1) / block_size());
-
-        set_blocks_free(index, n);
-     };
-
-   private:
-     const std::size_t block_size_;
-     const std::size_t block_count_;
-     std::byte* mem_data_{nullptr};
-     std::byte* mem_ledger_{nullptr};
-
-     inline void set_bit(std::byte* ledger, std::size_t bit_index) noexcept {
-        const std::size_t byte_idx = bit_index / 8;
-        const std::size_t bit_off = byte_idx % 8;
-        const std::byte mask = std::byte{1u} << bit_off;
-		ledger[byte_idx] |= mask;
-     }
-
-     inline void clear_bit(std::byte* ledger, std::size_t bit_index) noexcept {
-        const std::size_t byte_idx = bit_index / 8;
-        const std::size_t bit_off = byte_idx % 8;
-        const std::byte mask = std::byte{1u} << bit_off;
-        ledger[byte_idx] &= ~(mask);
-     }
-
-     std::size_t find_contiguous_blocks(std::size_t n) const noexcept; // TODO: impl
-
-     void set_blocks_in_use(std::size_t idx, std::size_t n) noexcept {
-        assert(idx + n <= block_count_);
-        for (std::size_t i = 0; i < n; ++i) {
-           set_bit(mem_ledger_, idx);
-        }
-     }
-
-     void set_blocks_free(std::size_t idx, std::size_t n) noexcept {
-        assert(idx + n <= block_count_);
-        for (std::size_t i = 0; i < n; ++i) {
-           clear_bit(mem_ledger_, idx);
-        }
-     }
+   // next/prev allow iter to prev/next contiguous mem region
+   BucketId prev_ = kInvalidBucketId; // starts at ptr - prev->size
+   BucketId next_ = kInvalidBucketId; // starts at ptr + size
 };
-
-template<std::size_t Id>
-struct bucket_descriptors;
-
- // TODO: Either define these per system or find a way to calculate them
-struct bucket_cfg16 {
-   static constexpr std::size_t block_size = 16;
-   static constexpr std::size_t block_count = 10000;
-};
-
-struct bucket_cfg32 {
-   static constexpr std::size_t block_size = 32;
-   static constexpr std::size_t block_count = 10000;
-};
-
-struct bucket_cfg64 {
-   static constexpr std::size_t block_size = 64;
-   static constexpr std::size_t block_count = 10000;
-};
-
-
-template<>
-struct bucket_descriptors<1> {
-   using type = std::tuple<bucket_cfg16, bucket_cfg32, bucket_cfg64>;
-};
-
-
-template<std::size_t id>
-using bucket_descriptor_t = typename bucket_descriptors<id>::type;
-
-template<std::size_t id>
-static constexpr std::size_t bucket_count = std::tuple_size<bucket_descriptor_t<id>>::value;
-
-template<std::size_t id>
-using pool_type = std::array<Bucket, bucket_count<id>>;
-
-template<std::size_t id, std::size_t Idx>
-struct get_size : std::integral_constant<std::size_t, std::tuple_element_t<Idx, bucket_descriptor_t<id>>::block_size> {};
-
-template<std::size_t id, std::size_t Idx>
-struct get_count : std::integral_constant<std::size_t, std::tuple_element_t<Idx, bucket_descriptor_t<id>>::block_count>{};
 
 #endif // POOL_H
