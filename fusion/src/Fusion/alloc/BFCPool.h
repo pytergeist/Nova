0e419eaa08c46a24e4cf9d398829af97de0aa5d9
pytergeist <tompopeworks@gmail.com>
2025-11-13 13:35:28 +0000

    changing pool to BFCPool, (Best Fit with Coalescing)

--- fusion/src/Fusion/alloc/BFCPool.h
+++ fusion/src/Fusion/alloc/BFCPool.h
@@ -6,32 +6,32 @@
 #include <utility>
 
 /* This header file includes code for the memory pool used by the pool
-allocator, the pool consists of bucketss, which point to mem regions - the
-buckets store various meta-data, which describes whether a bucket is in use,
+allocator, the pool consists of chunks, which point to mem regions - the
+chunks store various meta-data, which describes whether a chunk is in use,
 what the next/previous mem region is, the ptr to the curr mem region. */
 
-using BucketId = std::size_t;
-static const BucketId kInvalidBucketId = static_cast<BucketId>(-1);
+using ChunkId = std::size_t;
+static const ChunkId kInvalidChunkId = static_cast<ChunkId>(-1);
 
-struct Bucket {
+struct Chunk {
  public:
 
-   bool in_use() const noexcept { return std::cmp_not_equal(allocation_id_, kInvalidBucketId); }
+   bool in_use() const noexcept { return std::cmp_not_equal(allocation_id_, kInvalidChunkId); }
 
    std::size_t size() const noexcept { return size_; }
    std::size_t requested_size() const noexcept { return requested_size_; }
    std::int64_t allocation_id() const noexcept { return allocation_id_; }
 
-   void set_size(std::size_t bucket_size) noexcept { size_ = bucket_size; }
-   void set_requested_size(std::size_t req_bucket_size) noexcept {
-      requested_size_ = req_bucket_size;
+   void set_size(std::size_t chunk_size) noexcept { size_ = chunk_size; }
+   void set_requested_size(std::size_t req_chunk_size) noexcept {
+      requested_size_ = req_chunk_size;
    }
    void set_allocation_id(std::int64_t alloc_id) noexcept {
       allocation_id_ = alloc_id;
    }
 
-   BucketId prev() const noexcept { return prev_; }
-   BucketId next() const noexcept { return next_; }
+   ChunkId prev() const noexcept { return prev_; }
+   ChunkId next() const noexcept { return next_; }
 
  private:
    void *ptr = nullptr; // ptr to mem
@@ -40,8 +40,8 @@ struct Bucket {
    std::int64_t allocation_id_ = -1;
 
    // next/prev allow iter to prev/next contiguous mem region
-   BucketId prev_ = kInvalidBucketId; // starts at ptr - prev->size
-   BucketId next_ = kInvalidBucketId; // starts at ptr + size
+   ChunkId prev_ = kInvalidChunkId; // starts at ptr - prev->size
+   ChunkId next_ = kInvalidChunkId; // starts at ptr + size
 };
 
 #endif // POOL_H
--- fusion/src/Fusion/alloc/PoolAllocator.h
+++ fusion/src/Fusion/alloc/PoolAllocator.h
@@ -2,62 +2,8 @@
 #define POOL_ALLOCATOR_H
 
 #include <cstddef>
-#include "Pool.h"
-struct info {
-  std::size_t index{0};
-  std::size_t block_count{0};
-  std::size_t waste{0};
+#include "BFCPool.h"
 
-  bool operator<(const info &other) const noexcept {
-     return (waste == other.waste) ? block_count < other.block_count : waste < other.waste;
-  }
-};
-
-class PoolAllocator {
-   public:
-     PoolAllocator() = default;
-     ~PoolAllocator() = default;
-
-     template<std::size_t id, std::size_t... Idx>
-     auto& get_pool_instance(std::index_sequence<Idx...>) noexcept {
-        static pool_type<id> instance{{{get_size<id, Idx>::value, get_count<id, Idx>::value} ...}};
-        return instance;
-     }
-
-     template<std::size_t id>
-     auto& get_pool_instance() noexcept {
-        return get_pool_instance<id>(std::make_index_sequence<bucket_count<id>>());
-     }
-
-     template<std::size_t id>
-     [[nodiscard]] void *allocate(std::size_t bytes) {
-        auto& pool = get_pool_instance<id>();
-        std::array<info, bucket_count<id>> deltas;
-        std::size_t index = 0;
-        for (const auto& bucket: pool) {
-           deltas[index].index = index;
-           if (bucket.block_size() >= bytes) {
-           deltas[index].waste = bucket.block_size() - bytes;
-           deltas[index].block_count = 1;
-          } else {
-             const auto n = 1 + ((bytes - 1) / bucket.block_size());
-             const auto required_storage = n * bucket.block_size();
-             deltas[index].waste = required_storage - bytes;
-             deltas[index].block_count = n;
-          }
-          ++index;
-        }
-        std::sort(deltas.begin(), deltas.end()); // TODO: Change from std::sort as it can allocate
-
-        for (const auto &d: deltas) {
-           if (auto ptr = pool[d.index].allocate(bytes); ptr != nullptr) {
-              return ptr;
-           }
-        }
-        throw std::bad_alloc();
-     }
-
-};
 
 
 #endif // POOL_ALLOCATOR_H
