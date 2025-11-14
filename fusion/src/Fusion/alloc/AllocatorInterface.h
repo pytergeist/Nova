5363b6faefca60df91974c06e95b5d721a392ce8
pytergeist <tompopeworks@gmail.com>
2025-11-14 13:10:44 +0000

    adding ChunkComparator and Bucket structs to BFCPool

--- fusion/src/Fusion/alloc/AllocatorInterface.h
+++ fusion/src/Fusion/alloc/AllocatorInterface.h
@@ -3,11 +3,16 @@
 
 #include <memory>
 
+struct Chunk;
+using ChunkId = std::size_t;
+
 class IAllocator {
    public:
      IAllocator() = default;
      virtual void *allocate(std::size_t size) = 0;
      virtual void deallocate(void* p) = 0;
+
+     virtual const Chunk* ChunkFromId(const ChunkId cid) = 0;
 
    private:
      virtual ~IAllocator() = default;
--- fusion/src/Fusion/alloc/BFCPool.h
+++ fusion/src/Fusion/alloc/BFCPool.h
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <cstdint>
 #include <utility>
+#include <set>
+#include "AllocatorInterface.h"
 
 /* This header file includes code for the memory pool used by the pool
 allocator, the pool consists of chunks, which point to mem regions - the
@@ -26,9 +28,29 @@ struct Chunk {
 
    bool in_use() const noexcept { return std::cmp_not_equal(allocation_id, kInvalidChunkId); }
 };
+
+struct ChunkComparator {
+   public:
+     explicit ChunkComparator(IAllocator* allocator) : allocator_(allocator) {};
+     bool operator()(const ChunkId& ca, const ChunkId& cb) const {
+        const Chunk* a = allocator_->ChunkFromId(ca); // TODO: impl ChunkFromId
+        const Chunk* b = allocator_->ChunkFromId(cb);
+        if (a->size != b->size) {
+           return a->size < b->size;
+        }
+        return a->ptr < b->ptr;
+     }
+   private:
+     IAllocator* allocator_;
+};
+
+
+struct Bucket {
+   using FreeChunkSet = std::set<ChunkId, ChunkComparator>;
+   std::size_t bucket_size = 0;
+   FreeChunkSet free_chunks;
+
+   Bucket(IAllocator* allocator, std::size_t bs) : bucket_size(bs), free_chunks(ChunkComparator(allocator)) {};
+};
 // NOLINTEND(misc-non-private-member-variables-in-classes)
-
-
-struct Bucket {};
-
 #endif // BFC_POOL_H
