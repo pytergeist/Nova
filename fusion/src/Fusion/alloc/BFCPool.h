8ed9dc76e604d182e3fc62873853694a7a7b6638
pytergeist <tompopeworks@gmail.com>
2025-11-14 09:19:23 +0000

    Updating struct to follow cpp conventions with public data-members and NOLINT begin/end for non-private members

--- fusion/src/Fusion/alloc/BFCPool.h
+++ fusion/src/Fusion/alloc/BFCPool.h
@@ -1,5 +1,5 @@
-#ifndef POOL_H
-#define POOL_H
+#ifndef BFC_POOL_H
+#define BFC_POOL_H
 
 #include <cstddef>
 #include <cstdint>
@@ -13,35 +13,22 @@ what the next/previous mem region is, the ptr to the curr mem region. */
 using ChunkId = std::size_t;
 static const ChunkId kInvalidChunkId = static_cast<ChunkId>(-1);
 
+// NOLINTBEGIN(misc-non-private-member-variables-in-classes)
 struct Chunk {
- public:
-
-   bool in_use() const noexcept { return std::cmp_not_equal(allocation_id_, kInvalidChunkId); }
-
-   std::size_t size() const noexcept { return size_; }
-   std::size_t requested_size() const noexcept { return requested_size_; }
-   std::int64_t allocation_id() const noexcept { return allocation_id_; }
-
-   void set_size(std::size_t chunk_size) noexcept { size_ = chunk_size; }
-   void set_requested_size(std::size_t req_chunk_size) noexcept {
-      requested_size_ = req_chunk_size;
-   }
-   void set_allocation_id(std::int64_t alloc_id) noexcept {
-      allocation_id_ = alloc_id;
-   }
-
-   ChunkId prev() const noexcept { return prev_; }
-   ChunkId next() const noexcept { return next_; }
-
- private:
    void *ptr = nullptr; // ptr to mem
-   std::size_t size_ = 0;           // size of buffer
-   std::size_t requested_size_ = 0; // the client requested size of the buffer
-   std::int64_t allocation_id_ = -1;
+   std::size_t size = 0;           // size of buffer
+   std::size_t requested_size = 0; // the client requested size of the buffer
+   std::int64_t allocation_id = -1;
 
    // next/prev allow iter to prev/next contiguous mem region
-   ChunkId prev_ = kInvalidChunkId; // starts at ptr - prev->size
-   ChunkId next_ = kInvalidChunkId; // starts at ptr + size
-};
+   ChunkId prev = kInvalidChunkId; // starts at ptr - prev->size
+   ChunkId next = kInvalidChunkId; // starts at ptr + size
 
-#endif // POOL_H
+   bool in_use() const noexcept { return std::cmp_not_equal(allocation_id, kInvalidChunkId); }
+};
+// NOLINTEND(misc-non-private-member-variables-in-classes)
+
+
+struct Bucket {};
+
+#endif // BFC_POOL_H
--- fusion/src/Fusion/main.cpp
+++ fusion/src/Fusion/main.cpp
@@ -8,55 +8,11 @@
 #include "autodiff/Engine.h"
 #include "autodiff/EngineContext.h"
 #include "core/Device.h"
+#include "alloc/PoolAllocator.h"
 
-
-constexpr size_t num = 2048;
-constexpr size_t seed_num = 203;
-
-static auto rand_matrix_flat(size_t rows, size_t cols,
-                                    uint32_t seed = seed_num) -> std::vector<float> {
-   const size_t ndim = rows * cols;
-   std::vector<float> buf(ndim);
-   std::mt19937 rng(seed);
-   std::uniform_real_distribution<float> dist(0.0F, 1.0F);
-
-   for (size_t i = 0; i < ndim; ++i) {
-      buf[i] = dist(rng);
-      }
-   return buf;
-}
-
-
-auto main() -> int {
-   size_t a_seed = 42;
-   size_t b_seed = 43;
-   auto a_data = rand_matrix_flat(num, num, /*seed=*/a_seed);
-   auto b_data = rand_matrix_flat(num, num, /*seed=*/b_seed);
-//
-//   Tensor<float> A({N, N}, a_data, Device::CPU, true);
-//   Tensor<float> B({N, N}, a_data, Device::CPU, true);
-//
-//   Tensor<float> C = A + B;
-
-
-
-
-   const Tensor<float> A({num, num}, a_data, Device::CPU, false);
-   const Tensor<float> B({num, num}, b_data, Device::CPU, false);
-   Engine<float> engine;
-   EngineContext<float>::set(&engine);
-
-   // Warm up
-   volatile float guard = 0.f;
-   for (int i = 0; i < 3; ++i) {
-      auto C = A + B;
-      guard += C[0]; // prevent dead-code elimination if applicable
-   }
-
-   // Profile window
-   for (int i = 0; i < 50; ++i) {
-      auto C = A + B;
-      guard += C[0];
-   }
-   std::cout << (guard != 0.f) << "\n";
+int main() {
+   std::cout << "std::size_t: " << sizeof(std::size_t) << std::endl;
+   std::cout << "std::int64_t: " << sizeof(std::int64_t) << std::endl;
+   std::cout << "void*: " << sizeof(void*) << std::endl;
+   return 0;
 }
