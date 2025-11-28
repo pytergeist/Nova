7543ee92980e22f63f69633117d50b3e53538421
pytergeist <tompopeworks@gmail.com>
2025-11-28 15:57:18 +0000

    changing params in AutodiffMeta to be type erased using std::any

--- fusion/src/Fusion/autodiff/Traits.h
+++ fusion/src/Fusion/autodiff/Traits.h
@@ -5,6 +5,7 @@
 #include <initializer_list>
 #include <vector>
 #include <variant>
+#include <any>
 
 // TODO: Create fixed size AutodiffMeta for hot paths
 
@@ -18,10 +19,13 @@ struct NodeID {
 };
 
 template <typename T> struct AutodiffMeta {
-   using Param = std::variant<int, double, bool>;
+//   using Param = std::variant<int, double, bool>;
    std::vector<Tensor<T>> data; // TODO: migrate this away from std::vector
-   std::unordered_map<std::string, Param> params;
-
+//   std::unordered_map<std::string, Param> params;
+   // NB: in the current impl, params are type erased in meta
+   // but must be strongly typed at call site. This means strongtypes
+   // must be defined for each ops param type (curr defined in ops/OpParams.h)
+   std::any op_param;
    AutodiffMeta() = default;
 
    explicit AutodiffMeta(std::size_t n) { data.reserve(n); }
@@ -30,6 +34,9 @@ template <typename T> struct AutodiffMeta {
    AutodiffMeta &operator=(const AutodiffMeta &) = delete;
    AutodiffMeta(AutodiffMeta &&) noexcept = default;
    AutodiffMeta &operator=(AutodiffMeta &&) noexcept = default;
+
+   void emplace_back(const Tensor<T> &y) {data.emplace_back(y); }
+   void emplace_back(Tensor<T> &y) {data.emplace_back(y); }
 
    void push_back(const Tensor<T> &v) { data.emplace_back(v); }
    void push_back(Tensor<T> &&) = delete;
@@ -43,15 +50,15 @@ template <typename T> struct AutodiffMeta {
    Tensor<T> &operator[](std::size_t i) { return data.at(i); }
    const Tensor<T> &operator[](std::size_t i) const { return data.at(i); }
 
-   template <typename V> void set_param(const std::string &key, V value) {
-      params[key] = value;
-   }
+//   template <typename V> void set_param(const std::string &key, V value) {
+//      params[key] = value;
+//   }
 
-   template <typename V> V get_param(const std::string &key) const {
-      auto it = params.find(key);
-      FUSION_CHECK(it != params.end(), "Parameter not found");
-      return std::get<V>(it->second);
-   }
+//   template <typename V> V get_param(const std::string &key) const {
+//      auto it = params.find(key);
+//      FUSION_CHECK(it != params.end(), "Parameter not found");
+//      return std::get<V>(it->second);
+//   }
 
    auto begin() { return data.begin(); }
    auto end() { return data.end(); }
