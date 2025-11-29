b5c59f0d0eb68c1e0c542974555e2c7c63149e43
pytergeist <tompopeworks@gmail.com>
2025-11-29 20:13:29 +0000

    formatting/clang-tidying all comparison policies

--- fusion/src/Fusion/autodiff/policies/Comparison/Maximum.h
+++ fusion/src/Fusion/autodiff/policies/Comparison/Maximum.h
@@ -3,14 +3,15 @@
 
 #include <string_view>
 #include <vector>
-#include "Fusion/autodiff/AutodiffMode.h"
+
 #include "Fusion/TensorFactory.h"
+#include "Fusion/autodiff/AutodiffMode.h"
 #include "Fusion/autodiff/Traits.h"
 #include "Fusion/autodiff/policies/Operation.h"
 #include "Fusion/common/Checks.h"
 
 template <typename T> struct Maximum {
-   inline static constexpr std::string_view name = "Maximum";
+   static constexpr std::string_view name = "Maximum";
    using In = AutodiffMeta<T>;
    using Out = AutodiffMeta<T>;
    using GradIn = AutodiffMeta<T>;
@@ -20,9 +21,9 @@ template <typename T> struct Maximum {
       FUSION_CHECK(input.size() >= 2, "Maximum requires two inputs");
       FUSION_BOUNDS_CHECK(0, input.size());
       FUSION_BOUNDS_CHECK(1, input.size());
-      autodiff::NoGradGuard _;
-      const auto &a = input[0];
-      const auto &b = input[1];
+      const autodiff::NoGradGuard _;
+      const Tensor<T> &a = input[0];
+      const Tensor<T> &b = input[1];
       context.save("a", a);
       context.save("b", b);
       FUSION_ALLOW_SCALAR_BINARY(a, b);
@@ -33,14 +34,15 @@ template <typename T> struct Maximum {
    };
 
    GradIn backward(Context<T> &context, GradOut &grad_out) {
-      if (grad_out.size() == 0)
+      if (grad_out.empty()) {
          return {};
+      }
       FUSION_CHECK(grad_out.size() == 1,
                    "Maximum::backward expects exactly 1 upstream grad tensor");
-      autodiff::NoGradGuard _;
+      const autodiff::NoGradGuard _;
       const Tensor<T> &a = context.template load<Tensor<T>>("a");
       const Tensor<T> &b = context.template load<Tensor<T>>("b");
-      const auto &g0 = grad_out[0];
+      const Tensor<T> &g0 = grad_out[0];
       FUSION_CHECK(!g0.empty(), "Maximum::backward: upstream grad is empty");
       Tensor<T> c = g0 * (a >= b);
       Tensor<T> d = g0 * (b > a);
