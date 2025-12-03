a58c1f34c5b993a5a3b452194274a6d77da509ff
pytergeist <tompopeworks@gmail.com>
2025-12-03 16:42:13 +0000

    returning ADTensor from random module in Random PYBIND11 binding

--- fusion/src/Fusion/python/random/BindRandom.h
+++ fusion/src/Fusion/python/random/BindRandom.h
@@ -9,15 +9,20 @@
 
 namespace py = pybind11;
 
-template <typename T> void bind_random(py::module_ &m, const char *name) {
-   using PyT = Random<T>;
-   py::class_<PyT>(m, name)
-       .def(py::init([](const uint32_t &seed) { return PyT(seed); }))
-       .def(py::init([]() { return PyT(); }))
-       .def("uniform_cpp", &PyT::uniform,
-            "Create a uniform distribution of a shape between min and max "
-            "values",
-            py::arg("shape"), py::arg("min"), py::arg("max"));
-};
+template <typename T> void bind_random(py::module_ &m_ten, const char *name) {
+   using ADT = ADTensor<T>;
+   using Base = TensorBase<T>;
+   using RNG = Random<T>;
+
+   py::class_<RNG>(m_ten, name)
+       .def(py::init<uint32_t>(), py::arg("seed") = std::random_device{}())
+       .def(
+           "uniform_cpp",
+           [](RNG &self, const std::vector<size_t> &shape, T min, T max) {
+              Base b = self.uniform_base(shape, min, max);
+              return ADT(std::move(b), false);
+           },
+           py::arg("shape"), py::arg("min"), py::arg("max") );
+}
 
 #endif // BIND_RANDOM_H
