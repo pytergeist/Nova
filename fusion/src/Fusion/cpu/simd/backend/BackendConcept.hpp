1e3acc038a78ece9d33678a4b0cb02e77e3c699b
pytergeist <tompopeworks@gmail.com>
2025-12-22 17:18:13 +0000

    seperating BackendConcept into core, load/store, comparison & transcendental

--- fusion/src/Fusion/cpu/simd/backend/BackendConcept.hpp
+++ fusion/src/Fusion/cpu/simd/backend/BackendConcept.hpp
@@ -6,35 +6,44 @@
 /* TODO: refactor this into multiple concepts */
 
 template <typename B>
-concept BackendConcept =
+concept BackendCore = requires {
+   typename B::U;
+   typename B::vec;
+   typename B::wide_vec;
+
+   { B::kVectorBytes } -> std::convertible_to<std::size_t>;
+   { B::kLanes } -> std::convertible_to<std::size_t>;
+   { B::kUnroll } -> std::convertible_to<std::size_t>;
+   { B::kBlock } -> std::convertible_to<std::size_t>;
+
+   { B::kStepVec } -> std::convertible_to<std::size_t>;
+   { B::kStep } -> std::convertible_to<std::size_t>;
+};
+
+template <typename B>
+concept BackendLoadStore =
     requires(const typename B::U *ptr, typename B::U *out, typename B::vec v,
-             typename B::wide_vec wv, typename B::mask m) {
-       typename B::U;
-       typename B::vec;
-       typename B::wide_vec;
-
-       { B::kVectorBytes } -> std::convertible_to<std::size_t>;
-       { B::kLanes } -> std::convertible_to<std::size_t>;
-       { B::kUnroll } -> std::convertible_to<std::size_t>;
-       { B::kBlock } -> std::convertible_to<std::size_t>;
-
-       { B::kStepVec } -> std::convertible_to<std::size_t>;
-       { B::kStep } -> std::convertible_to<std::size_t>;
-
+             typename B::wide_vec wv) {
        { B::wide_load(ptr) } -> std::same_as<typename B::wide_vec>;
        { B::load(ptr) } -> std::same_as<typename B::vec>;
 
        { B::wide_store(out, wv) };
        { B::store(out, v) };
+    };
 
-       { B::cgt(v, v) } -> std::same_as<typename B::mask>;
-       { B::cge(v, v) } -> std::same_as<typename B::mask>;
-       {
-          B::duplicate(std::declval<typename B::U>())
-       } -> std::same_as<typename B::vec>;
+template <typename B>
+concept BackendComparison = requires(typename B::vec v, typename B::mask m) {
+   { B::cgt(v, v) } -> std::same_as<typename B::mask>;
+   { B::cge(v, v) } -> std::same_as<typename B::mask>;
+   {
+      B::duplicate(std::declval<typename B::U>())
+   } -> std::same_as<typename B::vec>;
+   { B::blend(m, v, v) } -> std::same_as<typename B::vec>;
+};
 
-       { B::blend(m, v, v) } -> std::same_as<typename B::vec>;
-
+template <typename B>
+concept BackendArithmetic =
+    requires(typename B::vec v, typename B::wide_vec wv) {
        { B::add(v, v) } -> std::same_as<typename B::vec>;
        { B::sub(v, v) } -> std::same_as<typename B::vec>;
        { B::mul(v, v) } -> std::same_as<typename B::vec>;
@@ -42,10 +51,18 @@ concept BackendConcept =
 
        { B::maximum(v, v) } -> std::same_as<typename B::vec>;
        { B::pow(v, v) } -> std::same_as<typename B::vec>;
-
-       { B::sqrt(v) } -> std::same_as<typename B::vec>;
-       { B::log(v) } -> std::same_as<typename B::vec>;
-       { B::exp(v) } -> std::same_as<typename B::vec>;
     };
+
+template <typename B>
+concept BackendTranscendental = requires(typename B::vec v) {
+   { B::sqrt(v) } -> std::same_as<typename B::vec>;
+   { B::log(v) } -> std::same_as<typename B::vec>;
+   { B::exp(v) } -> std::same_as<typename B::vec>;
+};
+
+template <typename B>
+concept BackendConcept =
+    BackendCore<B> && BackendLoadStore<B> && BackendComparison<B> &&
+    BackendArithmetic<B> && BackendTranscendental<B>;
 
 #endif // FUSION_CPU_BACKEND_CONCEPT_HPP
