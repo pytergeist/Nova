594470c335042a09c07caef9797330f1792095bc
pytergeist <tompopeworks@gmail.com>
2026-02-06 17:42:13 +0000

    adding physics layer to CMake build

---
+++ fusion/src/Fusion/physics/core/State.hpp
@@ -0,0 +1,27 @@
+#ifndef FUSION_PHYSICS_STATE
+#define FUSION_PHYSICS_STATE
+
+#include "Fusion/core/RawTensor.hpp"
+
+/*
+ * This file defines
+ *
+ *
+ */
+
+template <typename T> struct ParticlesSoA {
+   RawTensor<T> p, v, f, m; // pos, velocity, force, mass
+
+   static ParticlesSoA<T> from_raw_tensor(const RawTensor<T> &p,
+                                          const RawTensor<T> &v,
+                                          const RawTensor<T> &f,
+                                          const RawTensor<T> &m) {
+      ParticlesSoA<T> soa{p, v, f, m};
+      soa.validate();
+      return soa;
+   }
+
+   void validate() const;
+}
+
+#endif // FUSION_PHYSICS_STATE
