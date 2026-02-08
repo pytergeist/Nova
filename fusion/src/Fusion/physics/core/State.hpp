847ee6220bad539a850af85adc600197ff73d5df
pytergeist <tompopeworks@gmail.com>
2026-02-08 17:20:43 +0000

    adding CRS struct, for memory local neighbour storage

--- fusion/src/Fusion/physics/core/State.hpp
+++ fusion/src/Fusion/physics/core/State.hpp
@@ -1,24 +1,23 @@
 #ifndef FUSION_PHYSICS_STATE
 #define FUSION_PHYSICS_STATE
 
+#include <cstddef>
+#include <cstdint>
+
 #include "Fusion/core/RawTensor.hpp"
 
-/*
- * This file defines
- *
- *
- */
+// TODO: Remove template parameters from here! plans should be runtime objects
+// that pass around itemsize, not templated structs
 
-template <typename T>
-struct Vec3Ptrs {
-  const T* x;
-  const T* y;
-  const T* z;
+template <typename T> struct Vec3Ptrs {
+   const T *x;
+   const T *y;
+   const T *z;
 };
 
 struct EdgeList {
-   std::vector<uint32_t> i;
-   std::vector<uint32_t> j;
+   std::vector<std::uint32_t> i;
+   std::vector<std::uint32_t> j;
 
    // TODO: you're using just i here, need to set invariants of edge list,
    // i.e. assert (i.size() == j.size())
@@ -26,26 +25,42 @@ struct EdgeList {
    std::size_t size() const { return i.size(); }
 };
 
+struct CRS {
+   std::int64_t N = 0, E = 0;
+   std::vector<std::uint32_t> row_ptr;
+   std::vector<std::uint32_t> col_idx;
 
-// TODO: make this type a physics DType concept (should only be float, double, maybe half)
+   std::vector<float> w;
+   std::vector<float> r0;
+   std::vector<std::uint16_t> type;
+
+   bool sorted_by_j = false;
+   bool symmetric = false;
+   bool directed = false;
+};
+
+// TODO: make this type a physics DType concept (should only be float, double,
+// maybe half)
 template <typename T> struct ParticlesSoA {
    RawTensor<T> x, v, f, m; // position, velocity, force, mass
+   std::vector<std::uint32_t>
+       type; // This will eventually hold atom type by int
 
-   static ParticlesSoA<T> from_raw_tensor(const RawTensor<T> &p,
+   static ParticlesSoA<T> from_raw_tensor(const RawTensor<T> &x,
                                           const RawTensor<T> &v,
                                           const RawTensor<T> &f,
                                           const RawTensor<T> &m) {
-      ParticlesSoA<T> soa{p, v, f, m};
+      ParticlesSoA<T> soa{x, v, f, m};
 
-//      soa.validate(); // TODO: validate shapes
+      //      soa.validate(); // TODO: validate shapes
       return soa;
    }
 
-  Vec3Ptrs<T> vec3() const {
-    const T* base = x.get_ptr();
-    int n = N();
-    return { base + 0*n, base + 1*n, base + 2*n };
-  }
+   Vec3Ptrs<T> vec3() const {
+      const T *base = x.get_ptr();
+      int n = N();
+      return {base + 0 * n, base + 1 * n, base + 2 * n};
+   }
 
    void validate() const;
    T N() const { return static_cast<int>(x.shape()[1]); };
