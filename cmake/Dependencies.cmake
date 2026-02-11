446d552712c832f6da809a07d26536cf024512ea
pytergeist <tompopeworks@gmail.com>
2026-02-11 17:20:29 +0000

    temporarily removing gtest for build system

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -80,21 +80,21 @@ find_package(Eigen3 3.3 REQUIRED NO_MODULE)
 message(STATUS "Eigen3 include dir: ${EIGEN3_INCLUDE_DIR}")
 
 
-# ---------- Tests / GoogleTest ----------
-include(CTest)
-if(BUILD_TESTING)
-  find_package(GTest QUIET)
-  if(NOT GTest_FOUND)
-    message(STATUS "GTest not found; fetching via FetchContent")
-    FetchContent_Declare(
-            googletest
-            URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
-    )
-    set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
-    set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
-    FetchContent_MakeAvailable(googletest)
-  endif()
-endif()
+## ---------- Tests / GoogleTest ----------
+#include(CTest)
+#if(BUILD_TESTING)
+#  find_package(GTest QUIET)
+#  if(NOT GTest_FOUND)
+#    message(STATUS "GTest not found; fetching via FetchContent")
+#    FetchContent_Declare(
+#            googletest
+#            URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.zip
+#    )
+#    set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+#    set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+#    FetchContent_MakeAvailable(googletest)
+#  endif()
+#endif()
 
 # ---------- SLEEF (prefer installed; else fetch) ----------
 find_package(SLEEF CONFIG QUIET)
