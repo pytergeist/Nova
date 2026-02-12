12bffdfbbea34b4b24206b759aa54faf2eb9a200
pytergeist <tompopeworks@gmail.com>
2026-02-12 17:50:54 +0000

    adding block_rowwise_crs kernel to physics cpu layer

--- fusion/src/Fusion/physics/cpu/kernels/Vec3GatherSub.hpp
+++ fusion/src/Fusion/physics/cpu/kernels/Vec3GatherSub.hpp
@@ -30,4 +30,30 @@ void vec3_rowwise_crs(const Vec3Ptrs<T> &pos, const CRS &crs, T *out,
    }
 }
 
+
+template <typename T, class ParticleT>
+void block_rowwise_crs(const ParticleT &pos, const CRS &crs, T *out,
+                      std::uint64_t E) {
+   T *out_x = out + 0 * E;
+   T *out_y = out + 1 * E;
+   T *out_z = out + 2 * E;
+
+   for (std::uint32_t i = 0; i < crs.N; ++i) {
+      const T xi = pos.x_at(0, i);
+      const T yi = pos.x_at(1, i);
+      const T zi = pos.x_at(2, i);
+
+      const auto start = crs.row_ptr[i];
+      const auto end = crs.row_ptr[i + 1];
+
+      for (auto e = start; e < end; ++e) {
+         const auto j = crs.col_idx[e];
+
+         out_x[e] = xi - pos.x_at(0, j);
+         out_y[e] = yi - pos.x_at(1, j);
+         out_z[e] = zi - pos.x_at(2, j);
+      }
+   }
+}
+
 #endif // FUSION_PHYSICS_VEC3_GATHER_SUB
