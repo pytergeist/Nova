e1d4c0fbb9609f875d88251a145af6917434eb2e
pytergeist <tompopeworks@gmail.com>
2026-02-16 16:11:39 +0000

    adding naive initial impl of make_blocked_crs with ib_ptr, jb_ptr, jb_idx

--- fusion/src/Fusion/physics/core/PhysicsPlan.h
+++ fusion/src/Fusion/physics/core/PhysicsPlan.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "Neighbours.hpp"
 #include "State.hpp"
 
 enum class PairListKind { EdgeList, CRS, NMCLuster };
@@ -52,6 +53,104 @@ inline CRS make_crs(const ParticlesT &psoa, const EdgeList &edges) {
       crs.col_idx[pos] = dst;
    }
    return crs;
+}
+
+struct PairHash {
+   template <class T1, class T2>
+   std::size_t operator()(const std::pair<T1, T2> &p) const {
+      auto h1 = std::hash<T1>{}(p.first);
+      auto h2 = std::hash<T2>{}(p.second);
+      return h1 ^ h2;
+   }
+};
+
+struct PairEqual {
+   template <class T1, class T2>
+   bool operator()(const std::pair<T1, T2> &p1,
+                   const std::pair<T1, T2> &p2) const {
+      return p1.first == p2.first && p1.second == p2.second;
+   }
+};
+
+struct Group {
+   std::uint32_t group_idx;
+   std::uint32_t num_edges;
+   std::uint32_t ib_idx;
+   std::uint32_t jb_idx;
+};
+
+template <typename T, class ParticlesT>
+inline BlockedCRS make_blocked_crs(const ParticlesT &psoa,
+                                   EdgeList &edges) {
+   if (!edges.sorted) {
+      edges.sort();
+   }
+   BlockedCRS bcrs;
+   bcrs.N = psoa.N();
+   bcrs.E = edges.E();
+   bcrs.TILE = psoa.tile(); // TODO: curr just hardcoding for dev
+   bcrs.nBlocks = (bcrs.N + bcrs.TILE - 1) / bcrs.TILE;
+
+   bcrs.ib_ptr.assign(bcrs.nBlocks + 1, 0);
+   // TODO: this unordered map will have lots of hash collisions for large
+   // graphs. done now for simplicity, but change to something more efficient (std::vec? bitsetmask?)
+   std::unordered_map<std::pair<std::uint64_t, std::uint64_t>, std::uint64_t, PairHash, PairEqual> groups;
+   std::vector<std::uint32_t> group_counter;
+   for (std::size_t e = 0; e < bcrs.E; ++e) {
+      const std::uint32_t src = edges.i[e];
+      const std::uint32_t dst = edges.j[e];
+      std::uint64_t ib = src / bcrs.TILE;
+      std::uint64_t jb = dst / bcrs.TILE;
+      auto key = std::make_pair(ib, jb);
+      auto [it, inserted] = groups.try_emplace(
+          key, static_cast<std::uint64_t>(group_counter.size()));
+      if (inserted) {
+         group_counter.push_back(0);
+         group_counter[it->second] += 1;
+         bcrs.ib_ptr[ib + 1]++; // TODO: the below impl is sloppy af
+         bcrs.jb_idx.push_back(jb);
+      } else {
+         group_counter[it->second] += 1;
+      }
+
+      // compute lanes
+
+   }
+
+   for (std::size_t k = 1; k < bcrs.ib_ptr.size(); ++k) {
+		bcrs.ib_ptr[k] += bcrs.ib_ptr[k - 1];
+   }
+   bcrs.jb_ptr = group_counter;
+   bcrs.jb_ptr.insert(bcrs.jb_ptr.begin(), 0);
+
+   for (std::size_t k = 1; k < bcrs.jb_ptr.size(); ++k) {
+      bcrs.jb_ptr[k] += bcrs.jb_ptr[k - 1];
+   }
+
+
+   std::cout << "Groups: [";
+    for (auto v : group_counter) {
+         std::cout << v << ", ";
+      }
+	std::cout << "]" << std::endl;
+
+    std::cout << "ib_ptr: [";
+    for (auto i : bcrs.ib_ptr) {
+         std::cout << i << ", ";
+      }
+	std::cout << "]" << std::endl;
+    std::cout << "jb_idx: [";
+    for (auto i : bcrs.jb_idx) {
+         std::cout << i << ", ";
+      }
+	std::cout << "]" << std::endl;
+
+    std::cout << "jb_ptr: [";
+    for (auto i : bcrs.jb_ptr) {
+         std::cout << i << ", ";
+      }
+	std::cout << "]" << std::endl;
+   return bcrs;
 }
 
 template <typename T, class ParticlesT>
