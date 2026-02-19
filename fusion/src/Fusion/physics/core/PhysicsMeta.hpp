cf87fbe71669145faac0be331761d07eea546f18
pytergeist <tompopeworks@gmail.com>
2026-02-19 12:07:29 +0000

    removing const qualifiers from edgelist arg and changing psoa var name to particles

--- fusion/src/Fusion/physics/core/PhysicsMeta.hpp
+++ fusion/src/Fusion/physics/core/PhysicsMeta.hpp
@@ -15,13 +15,13 @@ template <typename T, class ParticlesT> struct PairwiseMeta {
 
 template <typename T, class ParticlesT>
 inline PairwiseMeta<T, ParticlesT> make_pairwise_meta(const ParticlesT &psoa,
-                                                      const EdgeList &edges) {
+                                                      EdgeList &edges) {
    PairwiseMeta<T, ParticlesT> meta;
    meta.fastpath = false;
    meta.fast_len = edges.E();
    meta.plan = make_pairwise_plan<T, ParticlesT>(psoa, edges);
    meta.out_shape = std::vector<std::size_t>{
-       meta.plan.psoa.dim(), static_cast<std::size_t>(meta.plan.E)};
+       meta.plan.particles.dim(), static_cast<std::size_t>(meta.plan.E)};
    meta.edges = edges;
    return meta;
 }
