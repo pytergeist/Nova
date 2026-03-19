c6cb5a5f89c4a4918fa2a6f0db2d04a494ee1564
pytergeist <tompopeworks@gmail.com>
2026-03-19 17:13:10 +0000

    updating physics/op/utils to use plan shape instead of duplicating shape in meta

--- fusion/src/Fusion/physics/ops/Utils.hpp
+++ fusion/src/Fusion/physics/ops/Utils.hpp
@@ -5,16 +5,15 @@
 #include "Fusion/physics/core/PhysicsPlanMeta.hpp"
 
 template <typename T, class ParticlesT>
-inline RawTensor<T> init_out_from_meta(const RawTensor<T> &x,
-                                       const PairwiseMeta<T, ParticlesT> &m) {
-   return RawTensor<T>(m.out_shape, x.dtype(), x.device());
+RawTensor<T> init_out_from_meta(const RawTensor<T> &x,
+                                const PairwiseMeta<T, ParticlesT> &m) {
+   return RawTensor<T>(m.plan.out_shape, x.dtype(), x.device());
 }
 
 template <typename T, class ParticlesT>
-inline RawTensor<T>
-init_out_from_meta(const RawTensor<T> &x,
-                   const GatherIndexMeta<T, ParticlesT> &m) {
-   return RawTensor<T>(m.out_shape, x.dtype(), x.device());
+RawTensor<T> init_out_from_meta(const RawTensor<T> &x,
+                                const GatherIndexMeta<T, ParticlesT> &m) {
+   return RawTensor<T>(m.plan.out_shape, x.dtype(), x.device());
 }
 
 #endif // FUSION_PHYSICS_OPS_UTILS_HPP
