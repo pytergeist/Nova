22e1d7b1c9b91b98ddfb81aac2d1bc866aa99fdf
pytergeist <tompopeworks@gmail.com>
2026-03-24 17:09:17 +0000

    adding descConstraints to be used in itemsize validation

---
+++ fusion/src/Fusion/core/fuir/DescContraints.h
@@ -0,0 +1,9 @@
+#ifndef FUSION_CORE_FUIR_DESC_CONTRAINTS_H
+#define FUSION_CORE_FUIR_DESC_CONTRAINTS_H
+
+enum class ItemSizeGroupConstraint {
+   HomogeneousItemSize,
+   TopologyAllowed,
+};
+
+#endif // FUSION_CORE_FUIR_DESC_CONTRAINTS_H
