d30b472dbfebf83a413e9ef98b1429c1f020ca52
pytergeist <tompopeworks@gmail.com>
2026-03-26 10:07:27 +0000

    adding initial impl of PairDelta3 (forward only) to physics autodiff layer

--- fusion/src/Fusion/physics/autodiff/ADPhysics.hpp
+++ fusion/src/Fusion/physics/autodiff/ADPhysics.hpp
@@ -5,17 +5,19 @@
 
 #include "Fusion/core/RawTensor.hpp"
 
+#include "Fusion/physics/core/PhysicsPlan.h"
 #include "Fusion/physics/core/PhysicsPlanMeta.hpp"
 #include "Fusion/physics/cpu/pairwise/PairwiseParams.hpp"
 #include "ParamPlan.hpp"
 #include "registry/pairwise/LJ.hpp"
+#include "registry/pairwise/PairDelta3.hpp"
 
 template <typename T, class ParticlesT>
 ADTensor<T> lj_energy(ADTensor<T> &x, ParticlesT &particles,
                       PairwiseMeta<T, ParticlesT> &meta, LJParams<T> params) {
    using Meta = PairwiseMeta<T, ParticlesT>;
    using Param = LJParamPlan<T, ParticlesT>;
-   Param pplan{meta, &particles, params};
+   Param pplan{meta, &particles, params}; // TODO: evaluate this
    return x.template unary_meta_hook<LennardJones<T, ParticlesT>, Meta, Param>(
        meta, pplan,
        [&](const RawTensor<T> &xb, Meta &meta_, const Param &plan_) {
@@ -24,4 +26,17 @@ ADTensor<T> lj_energy(ADTensor<T> &x, ParticlesT &particles,
        });
 }
 
+template <typename T, class ParticlesT>
+ADTensor<T> pair_delta3(ADTensor<T> &x, ParticlesT &particles,
+                        GatherIndexMeta<T, ParticlesT> &meta, NoParams params) {
+   using Meta = GatherIndexMeta<T, ParticlesT>;
+   using Param = GINoParamPlan<T, ParticlesT>;
+   Param pplan{meta, &particles, params};
+   return x.template unary_meta_hook<PairDelta3<T, ParticlesT>, Meta, Param>(
+       meta, pplan,
+       [&](const RawTensor<T> &xb, Meta &meta_, const Param &plan_) {
+          return pair_delta3_from_meta<T, ParticlesT>(xb, particles, meta_,
+                                                      plan_.params);
+       });
+}
 #endif // FUSION_PHYSICS_AUTODIFF_AD_PHYSICS_HPP
--- fusion/src/Fusion/physics/autodiff/ParamPlan.hpp
+++ fusion/src/Fusion/physics/autodiff/ParamPlan.hpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 
+#include "Fusion/physics/core/PhysicsPlan.h"
 #include "Fusion/physics/core/PhysicsPlanMeta.hpp"
 #include "Fusion/physics/cpu/pairwise/PairwiseParams.hpp"
 
@@ -10,6 +11,12 @@ template <typename T, class ParticlesT> struct LJParamPlan {
    PairwiseMeta<T, ParticlesT> meta;
    ParticlesT *particles;
    LJParams<T> params;
+};
+
+template <typename T, class ParticlesT> struct GINoParamPlan {
+   GatherIndexMeta<T, ParticlesT> meta;
+   ParticlesT *particles;
+   NoParams params;
 };
 
 #endif // FUSION_PHYSICS_REGISTRY_PARAM_PLAN_HPP
