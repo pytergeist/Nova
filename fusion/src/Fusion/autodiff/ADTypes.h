bf6ab52b8bb9ced5f974a60a2feaf151eadcd28a
pytergeist <tompopeworks@gmail.com>
2026-04-28 16:14:20 +0100

    making valueid/gradslotID hashable

--- fusion/src/Fusion/autodiff/ADTypes.h
+++ fusion/src/Fusion/autodiff/ADTypes.h
@@ -1,22 +1,47 @@
 #ifndef AD_TYPES_H_
 #define AD_TYPES_H_
 
+#include <coroutine>
 #include <cstddef>
 #include <cstdint>
-
-struct ValueID {
-   std::int64_t idx;
-   operator std::int64_t() const noexcept { return idx; }
-};
 
 struct NodeID {
    std::int64_t idx;
    operator std::int64_t() const noexcept { return idx; }
 };
 
-struct GradSlotID {
+struct ValueID {
    std::int64_t idx;
+
    operator std::int64_t() const noexcept { return idx; }
+
+   bool operator==(const ValueID& other) const noexcept {
+      return idx == other.idx;
+   }
+};
+
+template <>
+struct std::hash<ValueID> {
+   std::size_t operator()(const ValueID& s) const noexcept {
+      return std::hash<std::int64_t>{}(s.idx);
+   }
+};
+
+struct GradSlotID {
+   std::int64_t idx{-1};
+
+   operator std::int64_t() const noexcept { return idx; }
+
+   bool operator==(const GradSlotID& other) const noexcept {
+      return idx == other.idx;
+   }
+};
+
+template <>
+struct std::hash<GradSlotID> {
+   std::size_t operator()(const GradSlotID& s) const noexcept {
+      return std::hash<std::int64_t>{}(s.idx);
+   }
 };
 
 // NOLINTBEGIN(misc-non-private-member-variables-in-classes,
