b1e814f37aeba1c895b51e426e65277231981b56
pytergeist <tompopeworks@gmail.com>
2026-06-16 22:32:57 +0100

    adding niave impl of PairVector to ops/indexed

---
+++ fusion/src/Fusion/ops/indexed/PairVector.hpp
@@ -0,0 +1,17 @@
+#ifndef FUSION_OPS_INDEXED_PAIR_VECTOR_HPP
+#define FUSION_OPS_INDEXED_PAIR_VECTOR_HPP
+
+#include "Fusion/core/tensor/AoSoATensor.hpp"
+
+template <typename T, class ParticlesT>
+AoSoATensor<T> pair_delta3_from_meta(const AoSoATensor<T> &x, ParticlesT &p,
+                                   const GatherIndexMeta<T, ParticlesT> &meta,
+                                   NoParams params) {
+   AoSoATensor<T> out = init_out_from_meta(x, meta);
+   auto pv = make_view_x(p, x);
+   fusion::physics::iter::gather_index_tag<PairDelta3SIMD, T>(meta, pv, out,
+                                                              params);
+   return out;
+}
+
+#endif // FUSION_OPS_INDEXED_PAIR_VECTOR_HPP
