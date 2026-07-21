e6d1ac780a7696f089f7a6bdea501f262b2a92e8
Daniel Hewitt <dan.hewitt.15@ucl.ac.uk>
2026-07-21 17:27:13 +0100

    refactoring to use a dict rather than if statements for reduction functions

--- nova/src/losses/loss.py
+++ nova/src/losses/loss.py
@@ -1,6 +1,39 @@
 from abc import ABC, abstractmethod
+from enum import StrEnum
 
 from nova.src.backend.core import Tensor
+
+_REDUCTION_FUNCTIONS: dict = {}
+
+
+def _reduce_mean(values: Tensor, **kwargs) -> Tensor:
+    return values.sum() / values.size
+
+
+def _reduce_mean_with_sample_weight(
+    values: Tensor, sample_weights: Tensor | None = None
+):
+    if sample_weights is None:
+        raise ValueError("No sample weights provided")
+    else:
+        return values.sum() / sample_weights.sum()
+
+
+class ReductionMethod(StrEnum):
+    MEAN = "mean"
+    MEAN_WITH_SAMPLE_WEIGHT = "mean_with_sample_weight"
+
+    def apply(reduction_method, values: Tensor, sample_weights: Tensor | None = None):
+
+        return _REDUCTION_FUNCTIONS[reduction_method](
+            values, sample_weights=sample_weights
+        )
+
+
+_REDUCTION_FUNCTIONS.update({ReductionMethod.MEAN: _reduce_mean})
+_REDUCTION_FUNCTIONS.update(
+    {ReductionMethod.MEAN_WITH_SAMPLE_WEIGHT: _reduce_mean_with_sample_weight}
+)
 
 
 class Loss(ABC):
@@ -11,6 +44,15 @@ class Loss(ABC):
 
     def __init__(self, reduction_method: str = "mean"):
         self.reduction_method = reduction_method
+        self._is_valid_reduction_method(self.reduction_method)
+
+    @staticmethod
+    def _is_valid_reduction_method(reduction_method):
+        try:
+            ReductionMethod(reduction_method)
+            return
+        except ValueError:
+            raise ValueError(f"Invalid reduction method: {reduction_method}")
 
     @abstractmethod
     def call(
@@ -29,9 +71,7 @@ class Loss(ABC):
         values = self.call(*args, **kwargs)
         return self.reduce_loss(values)
 
-    def reduce_loss(
-        self, values: Tensor, sample_weights: Tensor | None = None
-    ) -> Tensor:  # TODO: make this method more generic
+    def reduce_loss(self, values) -> Tensor:  # TODO: make this method more generic
         """
         Reduce the loss values based on the specified reduction method.
         Loss is being reduced from a vector of sample errors to a single scalar that the optimiser can minimise.
@@ -43,15 +83,9 @@ class Loss(ABC):
         Returns:
             Reduced loss value scalar.
         """
-        if self.reduction_method == "mean":
-            loss = values.sum() / values.size
-        elif self.reduction_method == "mean_with_sample_weight":
-            if sample_weights is None:
-                raise ValueError("sample weights empty")
-            loss = values.sum() / sample_weights.sum()
-        else:
-            raise KeyError("invalid reduction method")
-        return loss
+        return ReductionMethod.apply(
+            reduction_method=self.reduction_method, values=values
+        )
 
     def __repr__(self):
         """
